1dd88dcc677b44dcd9ff605f2db9bb2fbc3711e8
Gerardo Lagunes García <g.lagunes@alumnos.upm.es>
2018-12-14 16:07:32 +0100

    Actualización de la lista de tipos semanticos a detectar

--- src/main/java/edu/ctb/upm/midas/constants/Constants.java
+++ src/main/java/edu/ctb/upm/midas/constants/Constants.java
@@ -66,26 +66,275 @@ public class Constants {
     /** Validaciones */
     public static final String SEMANTIC_TYPES[] = { "sosy", "diap", "dsyn", "fndg", "lbpr", "lbtr" };
     public static final List<String> SEMANTIC_TYPES_LIST = new ArrayList<String>(){{
+        //<editor-fold desc="old">
+        /*
         add("sosy");
+        //<editor-fold desc="Description">
+        *//*
+            DEF: An observable manifestation of a disease or condition based on clinical
+judgment, or a manifestation of a disease or condition which is experienced by
+the patient and reported as a subjective observation.
+            EX: Dyspnea; Nausea; Pain
+        *//*
+        //</editor-fold>
         add("diap");
+        //<editor-fold desc="Description">
+        *//*
+            DEF: A procedure, method, or technique used to determine the nature or identity
+of a disease or disorder.  This excludes procedures which are primarily carried
+out on specimens in a laboratory.
+            EX: Biopsy; Heart Auscultation; Magnetic Resonance Imaging
+        *//*
+        //</editor-fold>
         add("dsyn");
+        //<editor-fold desc="Description">
+        *//*
+            DEF: A condition which alters or interferes with a normal process, state, or
+activity of an organism.  It is usually characterized by the abnormal functioning
+of one or more of the host's systems, parts, or organs.  Included here is a
+complex of symptoms descriptive of a disorder.
+            EX: Diabetes Mellitus; Drug Allergy; Malabsorption Syndrome
+        *//*
+        //</editor-fold>
         add("fndg");
+        //<editor-fold desc="Description">
+        *//*
+            DEF: That which is discovered by direct observation or measurement of an
+organism attribute or condition, including the clinical history of the patient.
+The history of the presence of a disease is a 'Finding' and is distinguished
+from the disease itself.
+            EX: Birth History; Downward displacement of diaphragm; Decreased glucose level
+        *//*
+        //</editor-fold>
         add("lbpr");
+        //<editor-fold desc="Description">
+        *//*
+            DEF: A procedure, method, or technique used to determine the composition,
+quantity, or concentration of a specimen, and which is carried out in a clinical
+laboratory.  Included here are procedures which measure the times and rates of
+reactions.
+            EX: Blood Protein Electrophoresis; Crystallography; Radioimmunoassay
+        *//*
+        //</editor-fold>
         add("lbtr");
-
+        //<editor-fold desc="Description">
+        *//*
+            DEF: The outcome of a specific test to measure an attribute or to determine the
+presence, absence, or degree of a condition.
+            EX: Blood Flow Velocity; Serum Calcium Level; Spinal Fluid Pressure
+        *//*
+        //</editor-fold>
         add("inpr");//todas las siguientes para encontrar enfermedades mentales
+        //<editor-fold desc="Description">
+        *//*
+            DEF: A conceptual entity resulting from human endeavor.  Concepts assigned to
+this type generally refer to information created by humans for some purpose.
+            EX: Decision Support Techniques; Information Systems; Literature
+        *//*
+        //</editor-fold>
         add("menp");
+        //<editor-fold desc="Description">
+        *//*
+            DEF: A physiologic function involving the mind or cognitive processing.
+            EX: Anger; Auditory Fatigue; Avoidance Learning
+        *//*
+        //</editor-fold>
         add("mobd");
+        //<editor-fold desc="Description">
+        *//*
+            DEF: A clinically significant dysfunction whose major manifestation is
+behavioral or psychological.  These dysfunctions may have identified or presumed
+biological etiologies or manifestations.
+            EX: Agoraphobia; Cyclothymic Disorder; Frigidity
+        *//*
+        //</editor-fold>
         add("patf");
-
-//        add("fngs"); encuentra familias de hongos, eliminar
+        //<editor-fold desc="Description">
+        *//*
+            DEF: A disordered process, activity, or state of the organism as a whole, of a
+body system or systems, or of multiple organs or tissues.  Included here are
+normal responses to a negative stimulus as well as patholologic conditions or
+states that are less specific than a disease.  Pathologic functions frequently
+have systemic effects.
+            EX: Inflammation; Shock; Thrombosis
+        *//*
+        //</editor-fold>
         add("cgab");//para encontrar Coma
+        //<editor-fold desc="Description">
+        *//*
+            DEF: An abnormal structure, or one that is abnormal in size or location, present
+at birth or evolving over time as a result of a defect in embryogenesis.
+            EX: Albinism; Cleft palate with cleft lip; Polydactyly of toes
+        *//*
+        //</editor-fold>*/
+        //</editor-fold>
 
-//        add("clna");
+        //DISO|Disorders Metamap group
+        add("acab");//acab|T020|Acquired Abnormality
+        //<editor-fold desc="Description">
+        /*
+            DEF: An abnormal structure, or one that is abnormal in size or location, found
+in or deriving from a previously normal structure.  Acquired abnormalities are
+distinguished from diseases even though they may result in pathological
+functioning (e.g., "hernias incarcerate").
+            EX: Hemorrhoids; Hernia, Femoral; Cauliflower ear
+        */
+        //</editor-fold>
+        add("anab");//anab|T190|Anatomical Abnormality
+        //<editor-fold desc="Description">
+        /*
+            DEF: An abnormal structure, or one that is abnormal in size or location.
+            EX: Bronchial Fistula; Foot Deformities; Hyperostosis of skull
+        */
+        //</editor-fold>
+        add("comd");//comd|T049|Cell or Molecular Dysfunction
+        //<editor-fold desc="Description">
+        /*
+            DEF: A pathologic function inherent to cells, parts of cells, or molecules.
+            EX: DNA Damage; Wallerian Degeneration; Atypical squamous metaplasia
+        */
+        //</editor-fold>
+        add("cgab");//cgab|T019|Congenital Abnormality (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: An abnormal structure, or one that is abnormal in size or location, present
+at birth or evolving over time as a result of a defect in embryogenesis.
+            EX: Albinism; Cleft palate with cleft lip; Polydactyly of toes
+        */
+        //</editor-fold>
+        add("dsyn");//dsyn|T047|Disease or Syndrome (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: A condition which alters or interferes with a normal process, state, or
+activity of an organism.  It is usually characterized by the abnormal functioning
+of one or more of the host's systems, parts, or organs.  Included here is a
+complex of symptoms descriptive of a disorder.
+            EX: Diabetes Mellitus; Drug Allergy; Malabsorption Syndrome
+        */
+        //</editor-fold>
+        add("emod");//emod|T050|Experimental Model of Disease
+        //<editor-fold desc="Description">
+        /*
+            DEF: A representation in a non-human organism of a human disease for the purpose
+of research into its mechanism or treatment.
+            EX: Alloxan Diabetes; Liver Cirrhosis, Experimental; Transient Gene Knock-Out
+        */
+        //</editor-fold>
+        add("fndg");//fndg|T033|Finding (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: That which is discovered by direct observation or measurement of an
+organism attribute or condition, including the clinical history of the patient.
+The history of the presence of a disease is a 'Finding' and is distinguished
+from the disease itself.
+            EX: Birth History; Downward displacement of diaphragm; Decreased glucose level
+        */
+        //</editor-fold>
+        add("mobd");//mobd|T048|Mental or Behavioral Dysfunction (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: A clinically significant dysfunction whose major manifestation is
+behavioral or psychological.  These dysfunctions may have identified or presumed
+biological etiologies or manifestations.
+            EX: Agoraphobia; Cyclothymic Disorder; Frigidity
+        */
+        //</editor-fold>
+        add("neop");//neop|T191|Neoplastic Process
+        //<editor-fold desc="Description">
+        /*
+            DEF: A new and abnormal growth of tissue in which the growth is uncontrolled and
+progressive.  The growths may be malignant or benign.
+            EX: Abdominal Neoplasms; Bowen's Disease; Polyp in nasopharynx
+        */
+        //</editor-fold>
+        add("patf");//patf|T046|Pathologic Function (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: A disordered process, activity, or state of the organism as a whole, of a
+body system or systems, or of multiple organs or tissues.  Included here are
+normal responses to a negative stimulus as well as patholologic conditions or
+states that are less specific than a disease.  Pathologic functions frequently
+have systemic effects.
+            EX: Inflammation; Shock; Thrombosis
+        */
+        //</editor-fold>
+        add("sosy");//sosy|T184|Sign or Symptom (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: An observable manifestation of a disease or condition based on clinical
+judgment, or a manifestation of a disease or condition which is experienced by
+the patient and reported as a subjective observation.
+            EX: Dyspnea; Nausea; Pain
+        */
+        //</editor-fold>
 
-//        add("qlco");
-//        add("qnco");
-//        add("hcpp");
+        //PHEN|Phenomena Metamap group
+        add("lbpr");//lbpr|T059|Laboratory Procedure (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: A procedure, method, or technique used to determine the composition,
+quantity, or concentration of a specimen, and which is carried out in a clinical
+laboratory.  Included here are procedures which measure the times and rates of
+reactions.
+            EX: Blood Protein Electrophoresis; Crystallography; Radioimmunoassay
+        */
+        //</editor-fold>
+        add("lbtr");//lbtr|T034|Laboratory or Test Result (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: The outcome of a specific test to measure an attribute or to determine the
+presence, absence, or degree of a condition.
+            EX: Blood Flow Velocity; Serum Calcium Level; Spinal Fluid Pressure
+        */
+        //</editor-fold>
+
+        //PHYS|Physiology Metamap group
+        add("clna");//clna|T201|Clinical Attribute
+        //<editor-fold desc="Description">
+        /*
+            DEF: An observable or measurable property or state of an organism of clinical
+interest.
+            EX: Bone Density; heart rate; Range of Motion, Articular
+        */
+        //</editor-fold>
+        add("menp");//menp|T041|Mental Process (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: A physiologic function involving the mind or cognitive processing.
+            EX: Anger; Auditory Fatigue; Avoidance Learning
+        */
+        //</editor-fold>
+
+        //PROC|Procedures Metamap group
+        add("diap");//diap|T060|Diagnostic Procedure (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: A procedure, method, or technique used to determine the nature or identity
+of a disease or disorder.  This excludes procedures which are primarily carried
+out on specimens in a laboratory.
+            EX: Biopsy; Heart Auscultation; Magnetic Resonance Imaging
+        */
+        //</editor-fold>
+        add("lbpr");//lbpr|T059|Laboratory Procedure (YES)
+        //<editor-fold desc="Description">
+        /*
+            DEF: A procedure, method, or technique used to determine the composition,
+quantity, or concentration of a specimen, and which is carried out in a clinical
+laboratory.  Included here are procedures which measure the times and rates of
+reactions.
+            EX: Blood Protein Electrophoresis; Crystallography; Radioimmunoassay
+        */
+        //</editor-fold>
+
+
+
+
+
+
+
+
+
+
     }};
 
     /**
